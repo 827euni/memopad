e6ee55b9b62137041cfe77060af909d3a0b5c7d7
EUNA KWON <eunakwon@naver.com>
2024-12-09 15:25:56 +0900

    feat: 잘라내기, 복사, 붙여넣기 구현

--- memopad/memopad/Form1.cs
+++ memopad/memopad/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace memopad
 {
@@ -24,8 +25,6 @@ namespace memopad
         {
             InitializeComponent();
         }
-
-
 
         private void 다른이름으로저장ToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -101,6 +100,128 @@ namespace memopad
         {
             메모장 newMemo = new 메모장();
             newMemo.Show();
+        }
+
+        private void 시간날짜ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+
+            if(textBox.Text.Length == 0)
+            {
+                실행취소ToolStripMenuItem.Enabled = false;
+                잘라내기ToolStripMenuItem.Enabled = false;
+                삭제ToolStripMenuItem.Enabled = false;
+                bing으로검색ToolStripMenuItem.Enabled = false;
+                찾기ToolStripMenuItem.Enabled = false;
+                다음찾기ToolStripMenuItem.Enabled = false;
+                이전찾기ToolStripMenuItem.Enabled = false;
+                바꾸기ToolStripMenuItem.Enabled = false;
+            }
+
+            else
+            {
+                실행취소ToolStripMenuItem.Enabled = true;
+                잘라내기ToolStripMenuItem.Enabled = true;
+                삭제ToolStripMenuItem.Enabled = true;
+                bing으로검색ToolStripMenuItem.Enabled = true;
+                찾기ToolStripMenuItem.Enabled = true;
+                다음찾기ToolStripMenuItem.Enabled = true;
+                이전찾기ToolStripMenuItem.Enabled = true;
+                바꾸기ToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void 페이지설정ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 표시및유니코드ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 메모장_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 실행취소ToolStripMenuItem_Click(object sender, EventArgs e)
+        { // 왜 인지 모르게 진행이 안 됌.
+            if (textBox.CanUndo)
+            {
+                textBox.Undo();
+            }
+        }
+
+        private void 잘라내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox.SelectedText != "") {
+                textBox.Cut(); 
+            }
+        }
+
+        private void 복사ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox.SelectionLength > 0)
+            {
+                textBox.Copy();
+            }
+        }
+
+        private void 붙여넣기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox.Paste();
+        }
+
+        private void 삭제ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox.SelectedText = "";
+        }
+
+        private void 실행취소ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 잘라내기ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (textBox.SelectedText != "")
+            {
+                textBox.Cut();
+            }
+        }
+
+        private void 복사ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (textBox.SelectionLength > 0)
+            {
+                textBox.Copy();
+            }
+        }
+
+        private void 붙여넣기ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            textBox.Paste();
+        }
+
+        private void 삭제ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            textBox.SelectedText = "";
+        }
+
+        private void 모두선택ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            textBox.SelectAll();
         }
     }
 }
